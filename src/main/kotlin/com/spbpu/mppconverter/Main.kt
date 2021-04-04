92741ef4c1054c893801e4360ed2557948b18edb
kechin.vv <kechinv1@rambler.ru>
2021-04-05 01:59:03 +0300

    kootstrap submodule edit

--- src/main/kotlin/com/spbpu/mppconverter/Main.kt
+++ src/main/kotlin/com/spbpu/mppconverter/Main.kt
@@ -1,7 +1,11 @@
+package com.spbpu.mppconverter
+
 import com.spbpu.mppconverter.kootstrap.PSICreator
 import com.spbpu.mppconverter.util.debugPrint
+import org.jetbrains.kotlin.psi.KtFile
 
-fun main() {
-    val psi = PSICreator().getPSIForFile("test/HelloWorld.kt")
+fun main(arg: String): KtFile {
+    val psi = PSICreator().getPSIForFile(arg)
     psi.debugPrint()
+    return psi
 }
